952f01abfe0ddf334f0721af55c5689ee898954e
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2022-05-14 18:34:26 +0000

    Update version numbers and dates in model definition files
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/CoastalTools@94 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- CoastalTools.m
+++ CoastalTools.m
@@ -15,8 +15,8 @@ classdef CoastalTools < muiModelUI
 % 
     properties  (Access = protected)
         %implement properties defined as Abstract in muiModelUI
-        vNumber = '3.0'
-        vDate   = 'May 2021'
+        vNumber = '3.1'
+        vDate   = 'June 2022'
         modelName = 'CoastalTools'                        
         %Properties defined in muiModelUI that need to be assigned in setGui
         % ModelInputs  %classes required by model: used in isValidModel check 
