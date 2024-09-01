24d67daef14bb5252afb99c395fd7e8a40db7955
CoastalSEA <iantownend@outlook.com>
2024-09-01 17:44:29 +0100

    Updated License

--- app/help/coastaltools.m
+++ app/help/coastaltools.m
@@ -3,8 +3,7 @@
 % wave, water level and beach profile data. 
 
 %% Licence
-% The code is provided as Open Source code (issued under a GNU General 
-% Public License).
+% The code is provided as Open Source code (issued under a BSD 3-clause License).
 
 %% Requirements
 % CoastalTools is written in Matlab(TM) and requires v2016b, or later. In addition, 
